9806ace1d2fb8aea9fd3f3fd961fbc542841d90f
Lucas Ennes <lucassilvaennes@gmail.com>
2020-05-25 04:11:46 -0300

    configured eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,7 +10,7 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
